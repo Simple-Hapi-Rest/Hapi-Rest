a1380b3ff978b537827d6f9fc8e2b1d7b74df739
Mark Buchthal <mark.buchthal@gmail.com>
2016-04-24 19:51:59 -0700

    working post,delete,put requests. also remove unused routes

--- server.js
+++ server.js
@@ -11,22 +11,6 @@ server.connection({ port: config.server.port });
 
 server.route({
   method: 'GET',
-  path: '/',
-  handler: (request, reply) => {
-    reply('hello world!');
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/{name}',
-  handler: (request, reply) => {
-    reply('hello ' + encodeURIComponent(request.params.name) + '!');
-  }
-});
-
-server.route({
-  method: 'GET',
   path: '/jedi',
   handler: (request, reply) => {
     Jedi.find(null, (err, Jedi) => {
@@ -45,6 +29,57 @@ server.route({
       if (err) console.log('error getting heroes');
 
       reply(Hero);
+    });
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/hero/{id}',
+  handler: (request, reply) => {
+    Hero.findOne({ _id: request.params.id }, (err, Hero) => {
+      if (err) console.log('error getting single hero');
+
+      reply(Hero);
+    });
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/hero',
+  handler: (request, reply) => {
+    var newHero = new Hero(request.payload)
+    newHero.save((err, data) => {
+      if (err) console.log('error posting to heroes');
+
+      reply(data);
+    });
+  }
+});
+
+server.route({
+  method: 'DELETE',
+  path: '/hero/{id}',
+  handler: (request, reply) => {
+    Hero.remove({ _id: request.params.id}, (err) => {
+      if (err) console.log('error deleting from heroes');
+
+      reply('deleted');
+    });
+  }
+});
+
+server.route({
+  method: 'PUT',
+  path: '/hero/{id}',
+  handler: (request, reply) => {
+    var heroData = request.payload;
+    delete heroData._id;
+    Hero.findByIdAndUpdate({ _id: request.params.id }, heroData, (err) => {
+      if (err) console.log('error updating');
+
+      reply('updated');
     });
   }
 });
